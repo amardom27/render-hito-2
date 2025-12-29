860789a6f9555fea8cfb3d9eeb0a7841f2fe5719
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2025-12-29 22:29:01 +0100

    change port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import app from './app.js';
 
-const PORT = 3000;
+// process.env.PORT es la variable que usará Render.
+// Si no existe (en local), usará el puerto 3000 por defecto.
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor backend escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor backend escuchando en el puerto ${PORT}`);
 });
